bc0c8f7b0a41399fc8aa0b78919f7c8cac4b2ee7
Kanishk gaur <kgaur9412545040@gmail.com>
2025-11-28 18:54:07 +0530

    d

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add the 'images' object with 'unoptimized: true' to skip image optimization
+  images: {
+    unoptimized: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
